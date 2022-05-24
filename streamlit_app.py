106c421247ae437460093f266c79a44d9e560ba4
drpoirier18 <106194578+drpoirier18@users.noreply.github.com>
2022-05-24 15:25:44 -0600

    Update streamlit_app.py
    setting index to fruit name column

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,9 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
+#setting index to column name
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 #creating user interaction with a pick list, can pick the fruit they want
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
